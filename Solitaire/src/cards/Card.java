2221d5e006455e98ab1c896be8ef0dbd2193e8f0
roguelillith <roguelillith91@gmail.com>
2022-05-22 00:23:16 -0500

    Updated card file

--- Solitaire/src/cards/Card.java
+++ Solitaire/src/cards/Card.java
@@ -1,4 +1,15 @@
 package cards;
 
-class Card{
+public class Card{
+    public int house;
+    public boolean color;
+    public boolean shown;
+    public int value;
+
+    public Card( int house, boolean color, boolean shown, int value) {
+        this.house = house;
+        this.color = color;
+        this.shown = shown;
+        this.value = value;
+    }
 }
